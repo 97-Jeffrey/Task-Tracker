c45af1d7888ce579d3976193e927ea96055e53f1
Yu Hao Shao <yuhaoshao@YudeMacBook-Air.local>
2022-02-03 15:20:35 -0800

    able to delete a task and toggle task state

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,8 +1,8 @@
 <template>
-  <div :class="[task.reminder ? 'reminder' :'', 'task']">
+  <div @dblclick="$emit('toggle-reminder', task.id)" :class="[task.reminder ? 'reminder' :'', 'task']">
     <h3>
       {{ task.text }}
-      <i class="fas fa-times"></i>
+      <i @click="$emit('delete-task', task.id)" class="fas fa-times"></i>
     </h3>
     <p>{{ task.day }}</p>
   </div>
@@ -13,6 +13,8 @@
     name:"Task",
     props:{
       task:Object
+    },
+    methods:{
     }
   }
 </script>
@@ -27,7 +29,7 @@
     padding:10px 20px;
     cursor: pointer;
   }
-
+ 
   .task.reminder{
     border-left: 5px solid green;
   }
