764cf1ab564d26b85d8d7f7685ee69b1a8818987
Yu Hao Shao <yuhaoshao@YudeMacBook-Air.local>
2022-02-04 14:18:41 -0800

    addTask and toggle form complete

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,5 +1,5 @@
 <template>
-  <button v-on:click="onClick()" :style="{background: color}" class="btn"> {{ text }}</button>
+  <button @click="onClick()" :style="{background: color}" class="btn"> {{ text }}</button>
 </template>
 
 <script>
@@ -11,7 +11,7 @@
     },
     methods:{
       onClick(){
-        console.log(this.text)
+        this.$emit("btn-click")
       }
     }
   }
